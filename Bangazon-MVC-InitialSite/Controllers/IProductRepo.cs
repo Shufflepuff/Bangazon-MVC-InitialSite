c5fa601e607e9df0beda0eca35a73b3dbee9f50c
Stephen Szpak <srszpak@gmail.com>
2017-05-09 19:44:23 -0500

    added product repo controller and interface

---
+++ Bangazon-MVC-InitialSite/Controllers/IProductRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bangazon_MVC_InitialSite.Models;
+
+namespace Bangazon_MVC_InitialSite.Controllers
+{
+    public interface IProductRepo
+    {
+        Product GetProduct(int ProductId);
+        IEnumerable<Product> GetProducts();
+    }
+}
---
+++ Bangazon-MVC-InitialSite/Controllers/ProductRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bangazon_MVC_InitialSite.Models;
+
+namespace Bangazon_MVC_InitialSite.Controllers
+{
+    public class ProductRepo : IProductRepo
+    {
+        public Product GetProduct(int ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
