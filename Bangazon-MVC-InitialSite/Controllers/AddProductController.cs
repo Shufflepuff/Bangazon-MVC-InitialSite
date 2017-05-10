84f6d638ef694e59b417bb3383e54d8769ab921c
Taylor Shuff <tks2n@mtmail.mtsu.edu>
2017-05-10 18:49:44 -0500

    addproduct form stuff. almost working. error post.

--- Bangazon-MVC-InitialSite/Controllers/AddProductController.cs
+++ Bangazon-MVC-InitialSite/Controllers/AddProductController.cs
@@ -3,21 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bangazon_MVC_InitialSite.DAL;
 using Bangazon_MVC_InitialSite.Models;
 
 namespace Bangazon_MVC_InitialSite.Controllers
 {
     public class AddProductController : Controller
     {
+        readonly IAddProductRepo _addProductRepo;
+
+        public AddProductController(IAddProductRepo addProductRepo)
+        {
+            _addProductRepo = addProductRepo;
+        }
+
         // GET: AddProduct
         public ActionResult Index()
         {
-            var product = new Product();
-            product.Name = "example product";
-            product.ProductId = 1;
-            product.Price = 2;
-
             return View();
+        }
+
+        // POST: AddProduct
+        [HttpPost]
+        public ActionResult Index(Product product)
+        {
+            try
+            {
+                _addProductRepo.AddProduct(product);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return Redirect("Index");
+            }
         }
     }
 }
