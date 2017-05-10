8d5518c6e8bf11ea2aab4137900f03b2ed17cb2e
Alicia <akb.miceli@gmail.com>
2017-05-09 19:18:15 -0500

    added DAL folder

---
+++ Bangazon-MVC-InitialSite/DAL/NavbarRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bangazon_MVC_InitialSite.Controllers
+{
+    public class NavbarRepo
+    {
+    }
+}
