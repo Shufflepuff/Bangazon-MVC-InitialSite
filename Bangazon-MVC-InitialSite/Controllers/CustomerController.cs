bee1acb0a241887aa0082c7d67a26e4505960a74
Taylor Shuff <tks2n@mtmail.mtsu.edu>
2017-05-13 13:11:50 -0500

    Customer dropdown in top navbar.

---
+++ Bangazon-MVC-InitialSite/Controllers/CustomerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bangazon_MVC_InitialSite.DAL;
+using Bangazon_MVC_InitialSite.Models;
+
+namespace Bangazon_MVC_InitialSite.Controllers
+{
+    public class CustomerController : Controller
+    {
+        readonly ICustomerRepo _customerRepo;
+
+        public CustomerController(ICustomerRepo customerRepo)
+        {
+            _customerRepo = customerRepo;
+        }
+        // GET: Customer
+        public PartialViewResult CustomerDropDownPartial()
+        {
+           IEnumerable<Customer> customers =  _customerRepo.GetCustomers();
+
+            var dropdownData = customers
+                .Select(c => new SelectListItem()
+                {
+                    Text = c.Name, //Need to apply the correct text field here
+                    Value = c.CustomerId.ToString() //Need to apply the correct value field here
+                })
+                .ToList();
+
+            var model = new CustomerDropDown();
+            model.CustomerDropDownData = dropdownData;
+            model.SelectedId = 3;
+
+            return PartialView("~/Views/Customer/CustomerDropDownPartial.cshtml",model);
+        }
+
+        // GET: Customer
+        public PartialViewResult CustomerDropDownPartial(int id)
+        {
+            IEnumerable<Customer> customers = _customerRepo.GetCustomers();
+
+            var dropdownData = customers
+                .Select(c => new SelectListItem()
+                {
+                    Text = c.Name, //Need to apply the correct text field here
+                    Value = c.CustomerId.ToString() //Need to apply the correct value field here
+                })
+                .ToList();
+
+            var model = new CustomerDropDown();
+            model.CustomerDropDownData = dropdownData;
+            model.SelectedId = 3;
+
+            return PartialView("~/Views/Customer/CustomerDropDownPartial.cshtml", model);
+        }
+    } 
+}
---
+++ Bangazon-MVC-InitialSite/DAL/ICustomerRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bangazon_MVC_InitialSite.Models;
+
+namespace Bangazon_MVC_InitialSite.DAL
+{
+    public interface ICustomerRepo
+    {
+        IEnumerable<Customer> GetCustomers();
+    }
+}
